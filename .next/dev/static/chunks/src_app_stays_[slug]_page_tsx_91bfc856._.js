399cb1d40db69c9750ba9ee63dc29d769ee98867
Sandith Sithmaka Thenuwara <149329205+Sandith02@users.noreply.github.com>
2026-06-24 12:04:06 +0530

    Refactor code structure for improved readability and maintainability

--- .next/dev/static/chunks/src_app_stays_[slug]_page_tsx_91bfc856._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_9f6cdd78._.js",
-  "static/chunks/node_modules_69afa408._.js"
-],
-    source: "dynamic"
-});
